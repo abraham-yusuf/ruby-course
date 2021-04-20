fadcdb3b7f9fdaa085f12b6b3d628821f0f1f982
abraham-yusuf <1703020028@students.unis.ac.id>
2021-04-21 04:29:01 +0700

    add if statement

--- hello.rb
+++ hello.rb
@@ -1,8 +1,14 @@
 # Lesson 1: Outputting a String
 
-puts "Hello World!"
+#puts "Hello World!"
 
 # Lesson 2: Prompting User Input
 
-name = gets.chop
-puts "Hey, #{name}! Nice to meet you!"
+#name = gets.chop
+#puts "Hey, #{name}! Nice to meet you!"
+puts "What is your favorite color?"
+color = gets.chop
+puts "Wow! My favorite color is #{color} also!
+if color == "blue"
+ puts "Wow! My favorite color is #{color} also!"
+end
